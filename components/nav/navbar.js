1b9f3c225b4988f9dc6e5c64d138db15c92836cc
Keegan Leary <leary.keegan@gmail.com>
2022-08-28 13:22:39 +0900

    Dropdown signout

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -1,8 +1,61 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import styles from "./navbar.module.css";
+
 const Navbar = (props) => {
   const { username } = props;
+  const router = useRouter();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleOnClickHome = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
+  const handleShowDropdown = (e) => {
+    e.preventDefault();
+    setShowDropdown(!showDropdown);
+  };
+
   return (
-    <div>
-      Navbar <p>{username}</p>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink}>
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn} onClick={handleShowDropdown}>
+              <p className={styles.username}>{username}</p>
+            </button>
+
+            {showDropdown && (
+              <div className={styles.navDropdown}>
+                <div>
+                  <Link className={styles.linkName} href="/login">
+                    Sign out
+                  </Link>
+                  <div className={styles.lineWrapper}></div>
+                </div>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
