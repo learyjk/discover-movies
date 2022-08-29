3ac1041d4f4b5f969933bd10f0abda9aeae6a5fa
Keegan Leary <leary.keegan@gmail.com>
2022-08-29 21:52:37 +0900

    animate card

--- components/card/card.js
+++ components/card/card.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import styles from "./card.module.css";
 
 const Card = (props) => {
-  const { imgUrl = "/static/clifford.webp", size = "medium" } = props;
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
+    size = "medium",
+  } = props;
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
   const classMap = {
@@ -13,12 +17,14 @@ const Card = (props) => {
   };
   const handleOnError = () => {
     console.log("card image error");
-    setImgSrc("/static/clifford.webp");
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+    );
   };
 
   return (
     <div className={styles.container}>
-      <div className={classMap[size]}>
+      <motion.div whileHover={{ scale: 1.2 }} className={classMap[size]}>
         <Image
           src={imgSrc}
           alt="image"
@@ -26,7 +32,7 @@ const Card = (props) => {
           className={styles.cardImg}
           onError={handleOnError}
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
