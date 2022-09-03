3cfc903acea48a9e2432f4fbf80c28be6fcbb100
Keegan Leary <leary.keegan@gmail.com>
2022-09-03 06:55:40 -1000

    basic login scaffold

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,77 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [userMsg, setUserMsg] = useState("");
+
+  const router = useRouter();
+
+  const handleChangeEmail = (e) => {
+    setUserMsg("");
+    console.log("event", e);
+    setEmail(e.target.value);
+  };
+
+  const handleLoginWithEmail = async (e) => {
+    e.preventDefault();
+    console.log("hi button");
+
+    if (email) {
+      // route to dashboard
+      if (email === "leary.keegan@gmail.com") {
+        router.push("/");
+      } else {
+        console.log("Error logging in");
+      }
+    } else {
+      setUserMsg("Enter a valid email address");
+    }
+  };
+
   return (
     <div>
-      <Head>
+      <Head className={styles.container}>
         <title>Netflix SignIn</title>
       </Head>
+      <header className={styles.header}>
+        <div className={styles.headerWrapper}>
+          <Link className={styles.logoLink} href="/">
+            <a>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src="/static/netflix.svg"
+                  alt="Netflix logo"
+                  width="128px"
+                  height="34px"
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.mainWrapper}>
+          <h1 className={styles.signinHeader}>Sign In</h1>
+
+          <input
+            type="text"
+            placeholder="Email address"
+            className={styles.emailInput}
+            onChange={handleChangeEmail}
+          />
+
+          <p className={styles.userMsg}>{userMsg}</p>
+          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
+            Sign In
+          </button>
+        </div>
+      </main>
     </div>
   );
 };
